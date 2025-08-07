63d95073eb3ec8076f329744d5936f8a600efdf1
Ankan Kumar <Ankan.Kumar@LTTS.com>
2025-08-07 14:03:53 +0000

    commit code on server or get api for calling api

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-import express from 'express'
-import axios from 'axios'
-import dotevn from 'dotenv'
+import express from "express";
+import axios from "axios";
+import dotevn from "dotenv";
+
+dotevn.config();
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const BASE_URL = process.env.PPBASE_URL;
+const API_KEY = process.env.API_KEY;
 
 
-dotevn.config()
-const app=express()
-app.use(express.json())
+app.get("/api/stations", async (req, res) => {
+  try {
+     const {startDate,endDate}=req.query
+    const url = `${BASE_URL}/api/dcfc-data/station/all_stations`
+     console.log("Calling DEV API: ",url)
+     const response=await axios.get(url,{
+        params:{
+             "start-date": startDate,
+        "end-date": endDate,
+        },
+          headers: {
+        Authorization: `Bearer ${API_KEY}`
+      },
+     })
+     console.log("Status API: ",response.status)
 
-const PORT=process.env.PORT || 5000
-const BASE_URL=process.env.BASE_URL
-const API_KEY=process.env.API_KEY
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-app.listen(PORT,()=>{
-   console.log("server is running on port ",PORT);
-   
-})
+
+
+
+app.listen(PORT, () => {
+  console.log("server is running on port ", PORT);
+});
